6974711642bfa9ef3783d9498d5d468c761faf72
stepyen <stepyen@163.com>
2019-06-24 09:50:15 +0800

    Initial commit

---
+++ app/src/main/java/com/stepyen/xuidemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.stepyen.xuidemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ *
+ *
+ * 
+ * 慢慢来，整理自己需要的
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
